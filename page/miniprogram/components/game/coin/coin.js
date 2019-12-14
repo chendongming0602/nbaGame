bf555f402781c9e8a92c2050333d8e1fbc33fdd5
chendongming <chendm0602@163.com>
2019-12-14 21:15:47 +0800

    图片收集完成！

--- page/miniprogram/components/game/coin/coin.js
+++ page/miniprogram/components/game/coin/coin.js
@@ -1,4 +1,5 @@
 // components/game/coin/coin.js
+const APP=getApp();
 Component({
   /**
    * 组件的属性列表
@@ -39,7 +40,30 @@ Component({
       this.setData({ paos:arr[index]});
       setTimeout(()=>{
         this.setData({ isPao: false });
-      },3100)
+        if(index==0||index==1||index==5)
+        this.toasts(2);
+        else this.toasts(1)
+      },3000)
+    },
+    toasts(e){
+      let arr1=[
+        "看来硬币专家还是我~~~",
+        "你还需要多练练呢！",
+        "你怎么可能赢‘懂球’我呢！",
+        "你距离奖励还太远了！",
+      ];
+      let arr2=[
+        "你是不是偷偷补习了！",
+        "‘懂球’还会回来的~~~",
+        "奖励归你了！！！！",
+        "能告诉‘懂球’你的秘诀么...",
+      ];
+      let arrs = Math.floor(Math.random() * 4);
+      if(e==1){
+        APP.toastS(arr1[arrs])
+      }else{
+        APP.toastS(arr2[arrs])
+      }
     },
   }
 })
